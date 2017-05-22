46681a4a67f19e961c9fb00c0ae6ff9290f670d9
Daniel Krajnak <DanK@MVPsi.com>
2017-05-22 16:58:48 -0400

    create components

---
+++ src/app/resuts/resuts.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ResutsComponent } from './resuts.component';
+
+describe('ResutsComponent', () => {
+  let component: ResutsComponent;
+  let fixture: ComponentFixture<ResutsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ResutsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ResutsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/resuts/resuts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resuts',
+  templateUrl: './resuts.component.html',
+  styleUrls: ['./resuts.component.css']
+})
+export class ResutsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
